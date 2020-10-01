d9bf429365ddee17d08b124b94d0385be6d2a6cc
jarbas Spinelli <jfcspinelli@gmail.com>
2020-10-01 00:47:18 -0300

    Projeto Finalizado

---
+++ src/main/java/com/example/apicnpj/ApiCnpjApplication.java
@@ -0,0 +1,13 @@
+package com.example.apicnpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCnpjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCnpjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apicnpj/ApiCnpjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apicnpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCnpjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
